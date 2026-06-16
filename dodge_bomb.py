d1bba02644333b83e7bf2185e0e93397b2a6becf
koharu <c0a2508865@edu.teu.ac.jp>
2026-06-16 16:25:17 +0900

    追加機能１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
-
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -13,18 +13,60 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数：こうかとんRectかばくだんRect
-    戻り値：タプル（横方向判定結果，縦方向判定結果）
-    画面内ならTrue，画面外ならFalse
+    引数：効果トンRect or 爆弾Rect
+    戻り値：判定結果タプル（後方向判定結果、 縦方向判定結果）
+    True：画面内/False：画面外
     """
+
     yoko, tate = True, True
-    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+    if rct.left < 0 or WIDTH < rct.right:
         yoko = False
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
-    return yoko, tate
+    return yoko, tate        
+
+def gameover(screen: pg.Surface) -> None:
+    """
+    ゲームオーバー画面を5秒間表示する
+    """
+    # 黒い半透明の画面
+    black = pg.Surface((WIDTH, HEIGHT))
+    black.fill((0, 0, 0))
+    black.set_alpha(200)
+
+    # Game Over の文字
+    font = pg.font.Font(None, 80)
+    txt = font.render("Game Over", True, (255, 255, 255))
+    txt_rct = txt.get_rect(center=(WIDTH//2, HEIGHT//2 + 10))
+
+    # 泣いているこうかとん
+    cry_img = pg.transform.rotozoom(
+        pg.image.load("fig/8.png"), 0, 1.0
+    )
+    
+    left_rct = cry_img.get_rect(
+        center = (WIDTH//2 - 200, HEIGHT//2)
+    )
+
+    right_rct = cry_img.get_rect(
+        center = (WIDTH//2 + 200, HEIGHT//2)
+    )
+
+    # Surfaceへ描画
+    black.blit(txt, txt_rct)
+    black.blit(cry_img, left_rct)
+    black.blit(cry_img, right_rct)
+
+    # 画面へ表示
+    screen.blit(black, (0, 0))
+    pg.display.update()
+
+    # 5秒停止
+    time.sleep(5)
+    
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -37,10 +79,10 @@ def main():
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
-    bb_rct = bb_img.get_rect()
+    bb_rct = bb_img.get_rect() 
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = -5, +5
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -48,28 +90,30 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
-            print("ゲームオーバー")
+            gameover(screen)
             return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        #if key_lst[pg.K_UP]:
-        #    sum_mv[1] -= 5
-        #if key_lst[pg.K_DOWN]:
-        #    sum_mv[1] += 5
-        #if key_lst[pg.K_LEFT]:
-        #    sum_mv[0] -= 5
-        #if key_lst[pg.K_RIGHT]:
-        #    sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         for key, mv in DELTA.items():
-            if key_lst [key]:
+            if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
